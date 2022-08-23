fa53ec82a7888b44173be6937d631c3473833234
arinach <arina.tchukurova@yandex.ru>
2022-08-23 22:00:59 +0300

    add fiiir

---
+++ fiiir/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введи тут трехзначное число - ");
+int x;
+x = Convert.ToInt32(Console.ReadLine());
+string numbers = Convert.ToString(x);
+Console.WriteLine(numbers[1]);
