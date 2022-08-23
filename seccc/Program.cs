2fdac63dc86c53cd013bd60126a186933dad669b
arinach <arina.tchukurova@yandex.ru>
2022-08-23 22:39:57 +0300

    add secc

---
+++ seccc/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введи тут число - ");
+int x = Convert.ToInt32(Console.ReadLine());
+string index = Convert.ToString(x);
+int n = index.Length;
+if (n>2)
+{
+    Console.WriteLine(index[2]);
+}
+else
+{
+    Console.WriteLine("Тут ниче нет");
+}
